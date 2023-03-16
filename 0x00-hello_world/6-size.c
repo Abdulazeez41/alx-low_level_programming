7afbd2409f8810be10a6a20302713c531955137e
Abdulazeez41 <abdulazeezsalihu41@gmail.com>
2023-03-17 00:40:50 +0100

    Check data-type size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,10 @@
 
 int main(void)
 {
-	puts("Size of a char: %s byte(s)\n", sizeof(char));
-	puts("Size of an int: %d byte(s)\n", sizeof(int));
-	puts("Size of a long int: %d byte(s)\n", sizeof(unsigned int));
-	puts("Size of a long long int: %d byte(s)\n", sizeof(long));
-	puts("Size of a float: %d byte(s)\n", sizeof(float));
+	printf("Size of a char: %s byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(unsigned int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
