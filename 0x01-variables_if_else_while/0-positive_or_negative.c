82132783032720f3d3cd8f09875860abea334940
Abdulazeez41 <abdulazeezsalihu41@gmail.com>
2023-03-18 01:41:56 +0100

    Updated Positive or negative file to run with betty style

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,12 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 /* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+ *	main - Start program
+ *	Return: 0
+ */
 int main(void)
 {
 	int n;
@@ -13,12 +16,13 @@ int main(void)
 	/* your code goes there */
 	if (n > 0)
 	{
-		printf("%d is positive\n", n);
+		printf("%d is positive", n);
 	} else if (n == 0)
 	{
-		printf("%d is zero\n", n);
-	}else
-		printf("%d is negative\n", n);
+		printf("%d is zero", n);
+	} else
+		printf("%d is negative", n);
+
 	printf("\n");
 	return (0);
 }
