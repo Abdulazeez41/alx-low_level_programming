762792650a530c30e53e5bb8812bb1cee03f25e5
Abdulazeez41 <abdulazeezsalihu41@gmail.com>
2023-03-21 22:12:54 +0100

    Created Nested loops and function's folder with multiple task files

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *	main - Entry Point
+ *
+ *	Return: 0 if run successfully
+ */
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
