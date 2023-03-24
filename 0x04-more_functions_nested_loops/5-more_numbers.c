79f32dcbcafbb98db1d358cf9abd525b6059298c
Abdulazeez41 <abdulazeezsalihu41@gmail.com>
2023-03-25 00:57:50 +0100

    Updated Printing of more numbers file

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,21 +8,12 @@ void more_numbers(void)
 {
 	int i;
 	int j;
-	int k;
 
 	for (i = 0; i <= 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			k = 1;
-			if (j > 9)
-			{
-				_putchar(k + j);
-			}
-			else
-			{
-				_putchar('0' + j);
-			}
+			_putchar('0' + j);
 		}
 		_putchar('\n');
 	}
