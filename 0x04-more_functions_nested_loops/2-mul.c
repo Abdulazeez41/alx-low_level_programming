f0c863df3d776c1f4d7a205f1fdb6f4e8d87e128
Abdulazeez41 <abdulazeezsalihu41@gmail.com>
2023-03-24 23:28:27 +0100

    Added Multiply two integer function

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,11 +2,12 @@
 
 /**
  *	mul - Checks for upper letters
- *	@c: int data input
+ *	@a: int first data input
+ *	@b: int second data input
  *	Return: 1 if successful else print 0
  */
 
 int mul(int a, int b)
 {
-	return a * b;
+	return (a * b);
 }
