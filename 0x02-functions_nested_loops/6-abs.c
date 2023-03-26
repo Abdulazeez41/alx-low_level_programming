1f623512160503812f3d2f2349519d0bcb4c9bbc
Abdulazeez41 <abdulazeezsalihu41@gmail.com>
2023-03-27 00:25:29 +0100

    Added file to convert file to absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *	_abs - Computes an absolute value
+ *	@int: int data input
+ *	Return: 0 if run successful
+ */
+int _abs(int i)
+{
+	/*int i;*/
+
+	if (i < 0)
+	{
+		i = -i;
+		return (i);
+	}
+	else if (i > 0)
+	{
+		return (i);
+	}
+	else
+		return (0);
+}
