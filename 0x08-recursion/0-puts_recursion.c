f825c0e82ff3de08b57b4a88dc148d64bc88c530
Abdulazeez41 <abdulazeezsalihu41@gmail.com>
2023-04-06 00:05:27 +0100

    Initial commit with few 0x08-recursion task file

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *	_puts_recursion - print string with recursion.
+ *	@s: character input
+ *	Return: void
+ */
+void _puts_recursion(char *s)
+{
+	int i = 0;
+
+	if (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		s++;
+		_puts_recursion(s);
+	}
+}
