02bb4c42c0382641e36e3846792562a74ad88aac
Abdulazeez41 <abdulazeezsalihu41@gmail.com>
2023-04-06 02:42:56 +0100

    Added task 4 files

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *	_pow_recursion - print out the raise to the power
+ *	@x: integer input
+ *	@y: integer input
+ *	Return: -1 if encountered error
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x * y, y * 2));
+	}
+}
