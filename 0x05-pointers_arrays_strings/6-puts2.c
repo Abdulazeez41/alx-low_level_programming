64a94bef069df2a3434b6262658490a92d6f2587
Abdulazeez41 <abdulazeezsalihu41@gmail.com>
2023-04-10 17:51:13 +0100

    Updated puts file

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,12 +8,11 @@
  */
 void puts2(char *str)
 {
-	int i = 0;
+	int i;
 
-	while (*(str + i))
+	for (i = 0; str[i] != '\0'; i = i + 2)
 	{
-		_putchar(*(str + i));
-		i = i + 2;
+		_putchar(i + '0');
 	}
 	_putchar('\n');
 }
