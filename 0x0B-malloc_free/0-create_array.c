90068e67d24082bd8856b696aab326d3ab999be9
Abdulazeez41 <abdulazeezsalihu41@gmail.com>
2023-04-13 23:37:30 +0100

    Added file Create array file

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ *	char_array - Create an array of characters by allocating a memory
+ *	@size: integer input
+ *	@c: character input
+ *	Return: 0
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *ar;
+	unsigned int i;
+
+	ar = malloc(size * sizeof(char));
+	if (ar == NULL)
+		return NULL;
+
+	for (i = 0; i < size; i++)
+	{
+		ar[i] = c;
+	}
+	return ar;
+}
