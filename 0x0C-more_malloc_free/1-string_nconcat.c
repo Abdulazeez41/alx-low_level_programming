1d85579167efc9fa3354c6a2e7b9856f38c31bca
Abdulazeez41 <abdulazeezsalihu41@gmail.com>
2023-04-14 00:24:16 +0100

    Aded files for task 1 and 2

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ *	string_nconcat - Concatenate two strings
+ *	@s1: char input
+ *	@s2: char input
+ *	@n: integer input
+ *	Return: 0 if run successful
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+
+}
---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ *	_calloc_ - Allocate memory for an array using malloc
+ *	@nmemb: unsigned integer input
+ *	@size: unsigned integer input
+ *	Return: 0 if run successful
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+
+}
