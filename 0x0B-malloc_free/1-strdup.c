dbb1999da601a5d30f8f55c022c402ac9ce262eb
Abdulazeez41 <abdulazeezsalihu41@gmail.com>
2023-04-13 23:41:50 +0100

    Added task 2 (Copy string to allocated memory)

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ *	_strdup - Copies a string from a parameter to a new memory
+ *	@str: character input
+ *	Return: pointer
+ */
+char *_strdup(char *str)
+{
+	char *s;
+	int len;
+	int i;
+
+	if (str[0] == '\0')
+	{
+		return (NULL);
+	}
+	for (len = 0; str[len] != '\0'; len++)
+	{
+
+	}
+	s = malloc(len * sizeof(char));
+	if (s == NULL)
+		return (NULL);
+	for (i = 0; i < len; i++)
+	{
+		s[i] = str[i];
+	}
+	s[len] = '\0';
+	return (s);
+}
