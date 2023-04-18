cce33e49928cab9c01ef555bbd3e540e47c1fb72
Abdulazeez41 <abdulazeezsalihu41@gmail.com>
2023-04-18 04:27:38 +0100

    Added dog.h file

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,3 @@
+```text
+	0x0E-structures_typedef
+```
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ *	struct dog - About dogs
+ *	@name: dog name
+ *	@age: dog age
+ *	@owner: dog owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
