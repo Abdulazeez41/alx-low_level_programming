c2028006d152e9b392f99bd8d0f9db671c42cb8e
Abdulazeez41 <abdulazeezsalihu41@gmail.com>
2023-04-30 08:22:10 +0100

    Added Memory checked file

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,12 +1,18 @@
+#include "main.h"
 #include <stdlib.h>
-#include <stddef.h>
 
 /**
- *	malloc_checked - Allocate memory using malloc
- *	@b: unsigned intefer input
- *	Return: 0 if run successful
+ * malloc_checked - allocates memory using malloc
+ * @b: unsigned int
+ *
+ * Return: pointer
  */
 void *malloc_checked(unsigned int b)
 {
+	void *d;
 
+	d = malloc(b);
+	if (d == NULL)
+		exit(98);
+	return (d);
 }
