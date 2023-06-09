421690ce319d0001ae65977d87963290657f8749
Abdulazeez41 <abdulazeezsalihu41@gmail.com>
2023-06-09 02:14:46 +0100

    Added String length recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *	_strlen_recursion - print string length with recursion
+ *	@s: character input
+ *	Return: void
+ */
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (s[i] == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		return (_strlen_recursion(s + 1));
+	}
+}
