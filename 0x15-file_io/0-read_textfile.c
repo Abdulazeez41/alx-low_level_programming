9ba52e05f49d631732d1b64ed515091a49ac7608
Abdulazeez41 <abdulazeezsalihu41@gmail.com>
2023-07-20 17:52:59 +0100

    Updated Initial commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -6,33 +6,33 @@
 #include <stdlib.h>
 
 /**
- * my_read_and_print - reads a text file and print
- * @fname: read name of the file
- * @max_chars: no of char
+ * read_textfile - reads a text file and print
+ * @filename: read name of the file
+ * @letters: no of char
  *
  * Return: no of char
  */
-ssize_t my_read_and_print(const char *fname, size_t max_chars)
+ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int file_descriptor;
 	ssize_t bytes_read, bytes_written;
 	char *data_buffer;
 
-	if (fname == NULL)
+	if (filename == NULL)
 		return (0);
 
-	file_descriptor = open(fname, O_RDONLY);
+	file_descriptor = open(filename, O_RDONLY);
 	if (file_descriptor == -1)
 		return (0);
 
-	data_buffer = malloc(sizeof(char) * max_chars);
+	data_buffer = malloc(sizeof(char) * letters);
 	if (data_buffer == NULL)
 	{
 		close(file_descriptor);
 		return (0);
 	}
 
-	bytes_read = read(file_descriptor, data_buffer, max_chars);
+	bytes_read = read(file_descriptor, data_buffer, letters);
 	close(file_descriptor);
 
 	if (bytes_read == -1)
