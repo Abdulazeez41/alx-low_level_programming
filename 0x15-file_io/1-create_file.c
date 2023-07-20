66179c549b3feaa6aa41d71fc1c2376417113488
Abdulazeez41 <abdulazeezsalihu41@gmail.com>
2023-07-20 18:14:37 +0100

    Updated file creator

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,11 +8,11 @@
 /**
  * create_file - creates a file.
  * @filename: name of the file to create
- * @content: char content
+ * @text_content: char text_content
  *
  * Return: 1 on success, -1 on failure
  */
-int create_file(const char *filename, char *content)
+int create_file(const char *filename, char *text_content)
 {
     int new_fd;
 	ssize_t new_len = 0;
@@ -22,8 +22,8 @@ int create_file(const char *filename, char *content)
 	new_fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (new_fd == -1)
 		return (-1);
-	if (content != NULL)
-		new_len = write(new_fd, content, _countStr(content));
+	if (text_content != NULL)
+		new_len = write(new_fd, text_content, _countStr(text_content));
 	close(new_fd);
 	if (new_len == -1)
 		return (-1);
