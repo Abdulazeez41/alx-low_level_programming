067382544251f52868019a5dc195654606f71a5b
Abdulazeez41 <abdulazeezsalihu41@gmail.com>
2023-07-23 23:10:39 +0100

    Renamed pow file

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -29,3 +29,25 @@ void print_binary(unsigned long int n)
 		divisor >>= 1;
 	}
 }
+
+/**
+ * _pow - raise to the power
+ * @base: base of the exponent
+ * @power: power of the exponent
+ *
+ * Return: value of (base ^ power)
+ */
+unsigned long int _pow(unsigned int base, unsigned int power)
+{
+	unsigned long int result;
+	unsigned int i;
+
+	result = 1;
+	i = 1;
+	while (i <= power)
+	{
+		result *= base;
+		i++;
+	}
+	return (result);
+}
