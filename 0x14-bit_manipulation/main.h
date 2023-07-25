1ddaa784dfb95345538878e35bc6de08e0f6b52d
Abdulazeez41 <abdulazeezsalihu41@gmail.com>
2023-07-25 16:40:12 +0100

    Added Clear bit file

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -7,5 +7,6 @@ unsigned long int _pow(unsigned int base, unsigned int power);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
 
 #endif /* _MAIN_H_ */
