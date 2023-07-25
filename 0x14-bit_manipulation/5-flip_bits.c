cffb38c312ee9fefce6f45b01755742fa353078a
Abdulazeez41 <abdulazeezsalihu41@gmail.com>
2023-07-25 21:43:31 +0100

    Updated flip bit file

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -9,19 +9,19 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-    unsigned long int difference;
-    unsigned long int check;
-    unsigned int i;
-    unsigned int count;
+	unsigned long int difference;
+	unsigned long int check;
+	unsigned int i;
+	unsigned int count;
 
-    count = 0;
-    check = 1;
-    difference = n ^ m;
-    for (i = 0; i < (sizeof(unsigned long int) * 8); i++)
-    {
-        if ((difference & check) != 0)
-            count++;
-        check <<= 1;
-    }
-    return count;
+	count = 0;
+	check = 1;
+	difference = n ^ m;
+	for (i = 0; i < (sizeof(unsigned long int) * 8); i++)
+	{
+		if ((difference & check) != 0)
+			count++;
+		check <<= 1;
+	}
+	return (count);
 }
