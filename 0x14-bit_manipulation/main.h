73cecd1b7b25356b7cc1c6833ffd3ea0b2478bf5
Abdulazeez41 <abdulazeezsalihu41@gmail.com>
2023-08-04 00:34:29 +0100

    Added get endianness file

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -8,5 +8,6 @@ unsigned long int _pow(unsigned int base, unsigned int pow);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
+int get_endianness(void);
 
 #endif
