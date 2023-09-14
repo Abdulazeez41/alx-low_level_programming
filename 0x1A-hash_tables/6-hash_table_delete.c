0767e486700aeef2042b91578bf41744fa782b3d
Abdulazeez41 <abdulazeezsalihu41@gmail.com>
2023-09-14 12:12:20 +0100

    Added a function that deletes a hash table file

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -6,21 +6,21 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
-hash_node_t *nd, *tmp;
-unsigned long int counter = 0;
+	hash_node_t *nd, *tmp;
+	unsigned long int counter = 0;
 
-for (counter = 0; counter < ht->size; counter++)
-{
-nd = ht->array[counter];
-while (nd != NULL)
-{
-tmp = nd;
-nd = nd->next;
-free(tmp->key);
-free(tmp->value);
-free(tmp);
-}
-}
-free(ht->array);
-free(ht);
+	for (counter = 0; counter < ht->size; counter++)
+	{
+		nd = ht->array[counter];
+		while (nd != NULL)
+		{
+			tmp = nd;
+			nd = nd->next;
+			free(tmp->key);
+			free(tmp->value);
+			free(tmp);
+		}
+	}
+	free(ht->array);
+	free(ht);
 }
